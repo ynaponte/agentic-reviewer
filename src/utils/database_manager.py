ac4ed36a0980b7f753718399718efafa623cf3f8
Ynã Ponte <ynaqp@alu.ufc.br>
2025-02-05 15:46:16 -0300

    Trocado a maneira que artigos são carregados: não mais permite documentos duplicados; adição de novos metadados; melhora na eficiência. Método "query" agora permite busca em multiplas sources e/ou uploaders. Adição de um método de busca através dos metadados.

--- src/utils/database_manager.py
+++ src/utils/database_manager.py
@@ -7,6 +7,7 @@ from langchain_core.documents import Document
 from typing import List, Dict, Optional
 from threading import Lock
 import os
+import hashlib
 import uuid
 
 
@@ -54,60 +55,67 @@ class VectorDatabaseManager:
         """Carrega documentos com chunking e metadados"""
         if not self.vectorstore:
             raise RuntimeError("Database not initialized. Call initialize_db() first.")
-
-        doc_loaded_count = 0
         
         for filename in os.listdir(directory):
             if not filename.endswith(".pdf"):
                 continue
-            
-            doc_id = str(uuid.uuid4())
 
             try:
+                source = filename[:-4].lower()  # Retira a extensão .pdf do nome do artigo e deixa em caixa baixa
+                doc_id = self._gen_doc_id(source)
+
+                # Checa se o documento já existe no banco de dados
+                if self.get_document_metadata(doc_id=doc_id)['metadatas']:
+                    continue
+
                 loader = PyMuPDFLoader(os.path.join(directory, filename))
-                pages = loader.load()
+                pages = loader.load_and_split(text_splitter=self.text_splitter)  # Retorna uma lista de chunks, contendo metadata do documento
+                total_of_chunks = len(pages)
+
+                for chunk_idx, page_chunk in enumerate(pages):
+                    page_chunk.metadata.update({
+                        "doc_id":doc_id,
+                        "source": filename,
+                        "uploader": uploader,
+                        "chunk_idx": chunk_idx,
+                        "total_chunks": total_of_chunks,
+                        "last_modified": datetime.now().isoformat()
+                    })
+                    page_chunk.id = str(uuid.uuid4())
                 
-                for page in pages:
-                    processed_docs = self._process_document(
-                        source=filename,
-                        uploader=uploader,
-                        page=page.metadata['page'] + 1,
-                        total_pages=page.metadata['total_pages'],
-                        content=page.page_content
-                    )
-                    
-                    self.vectorstore.add_documents(
-                        documents=processed_docs,
-                        ids=doc_id
-                    )        
-                    doc_loaded_count += len(processed_docs)
+                self.vectorstore.add_documents(
+                    documents=pages
+                )
                 
             except Exception as e:
                 print(f"Erro processando {filename}: {str(e)}")
                 continue
-        
-        return doc_loaded_count
     
     def query(
         self,
         query: Optional[str] = "",
-        uploader: Optional[str] = None,
-        source: Optional[str] = None,
+        uploader: Optional[List[str]] = None,
+        source: Optional[List[str]] = None,
         top_k: Optional[int] = 10
     ) -> List[Document]:
         
         """
         Consulta flexível em todos os artigos da base de dados,
         possibilitando utilizar todos os artigos para obteção de informações específicas.
-        Também permite a filtragem por uploader e por artigos específicos.
+        Também permite a filtragem por uploader e por artigos específicos. Caso especificado
+        uploader e/ou source, limita os artigos da consulta a apenas aqueles com tais dados
+        em seus metadados.
         """
         
         if not self.vectorstore:
             raise RuntimeError("Database not initialized. Call initialize_db() first.")
-            
+        
+        # Filtros para pesquisa avançada    
         filters = [
-            {filter_argument: value} for filter_argument, value in (("uploader", uploader), ("source", source))
-            if value is not None
+            {filter_argument: {"$in": value}} for filter_argument, value in (
+                ("uploader", uploader),
+                ("source", source)
+            )if value is not None
         ]
 
         chroma_filters = None if filters == [] else (
@@ -122,45 +130,46 @@ class VectorDatabaseManager:
 
         return [self._format_output(doc) for doc in query_output]
     
-    def switch_current_collection(self, new_collection) -> 'VectorDatabaseManager':
-        """Muda a coleção atual para a nova coleção"""
+    def dump_doc_by_meta(
+        self, 
+        doc_id: Optional[str] = None,
+        source: Optional[str] = None,
+        uploader: Optional[str] = None,
+        page: Optional[int] = None,
+    ) -> List[Document]:
+        """
+        Método para obter as chunks de um artigo específico, identificado pelo nome(source).
+        Se o parâmetro page for None, retorna todos os chunks (documento completo).
+        Se page for um número, retorna somente os chunks correspondentes àquela página.
+        """
 
-        if new_collection is not self.vectorstore._collection_name:
-            self.vectorstore = Chroma(
-                collection_name=new_collection,
-                embedding_function=self.embedding,
-                persist_directory=self.vectorstore._persist_directory,
-            )
+        if not self.vectorstore:
+            raise RuntimeError("Database not initialized. Call initialize_db() first.")
+        
+        filters = [
+            {filter_argument: spec} for filter_argument, spec in (
+                ("doc_id", {"$eq": doc_id}),
+                ("source", {"$eq": source}),
+                ("uploader", {"$eq": uploader}),
+                ("page", {"$eq": page})
+            )if spec['$eq'] is not None
+        ]
 
-        return self
+        where_filters = {"$and": filters} if len(filters) > 1 else filters[0]
+
+        doc_search_result = self.vectorstore._collection.get(
+            where=where_filters,
+            include=['metadatas']
+        )
+
+        return doc_search_result
 
     # ------------------------ Private Methods ------------------------
 
-    def _process_document(
-            self,
-            source: str,
-            uploader: str, 
-            page: int, 
-            total_pages: int, 
-            content: str
-    ) -> List[Document]:
-        """Processa a página de um documento, adicionando metadados e fatiando-a em chuncks"""
-        base_metadata = {
-            "source": source,
-            "uploader": uploader,
-            "page": page,
-            "total_pages": total_pages,
-            "last_modified": datetime.now().isoformat()
-        }
-
-        chunks = self.text_splitter.split_text(content)
-        return [
-            Document(
-                page_content=chunk,
-                metadata={**base_metadata, "chunk_id": f"{i:04d}"}
-            )
-            for i, chunk in enumerate(chunks)
-        ]
+    @staticmethod
+    def _gen_doc_id(filename: str) -> str:
+        """Gera um id para o documento, a partir do seu nome"""
+        return hashlib.sha256(filename.encode('utf-8')).hexdigest()
     
     @staticmethod
     def _format_output(doc: Document) -> Dict:
@@ -169,4 +178,9 @@ class VectorDatabaseManager:
             "content": doc.page_content,
             "source": doc.metadata.get("source"),
             "uploader": doc.metadata.get("uploader"),
+            "page": doc.metadata.get("page"),
+            "total_pages": doc.metadata.get("total_pages"),
+            "doc_id": doc.metadata.get("doc_id"),
+            "chunck_idx": doc.metadata.get("chunck_idx"),
+            "total_chunks": doc.metadata.get("total_chunks"),
         }
