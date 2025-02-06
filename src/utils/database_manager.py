196243fad035ccb691e3f731103a19bd07d3d230
Ynã Ponte <ynaqp@alu.ufc.br>
2025-02-06 16:14:26 -0300

    Adicionados métodos para reformatação da saída da pesquisa por metadados/conteúdo de documentos inteiros. Implementado a retirada de metadados inuteis/vazios que o objeto de carregamento dos PDFs gera.

--- src/utils/database_manager.py
+++ src/utils/database_manager.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any
 from threading import Lock
 import os
 import hashlib
@@ -51,7 +51,7 @@ class VectorDatabaseManager:
         self.current_collection = collection_name
         return self
     
-    def store_documents(self, directory: str, uploader: str) -> int:
+    def store_documents(self, directory: str, uploader: str) -> 'VectorDatabaseManager':
         """Carrega documentos com chunking e metadados"""
         if not self.vectorstore:
             raise RuntimeError("Database not initialized. Call initialize_db() first.")
@@ -65,12 +65,25 @@ class VectorDatabaseManager:
                 doc_id = self._gen_doc_id(source)
 
                 # Checa se o documento já existe no banco de dados
-                if self.get_document_metadata(doc_id=doc_id)['metadatas']:
+                if self.search_doc_by_meta(doc_id=doc_id):
                     continue
 
                 loader = PyMuPDFLoader(os.path.join(directory, filename))
                 pages = loader.load_and_split(text_splitter=self.text_splitter)  # Retorna uma lista de chunks, contendo metadata do documento
                 total_of_chunks = len(pages)
+                metadata_to_delete = [
+                    'author',
+                    'creationDate',
+                    'creator',
+                    'file_path'
+                    'format',
+                    'keywords',
+                    'modDate',
+                    'producer',
+                    'subject',
+                    'title',
+                    'trapped'
+                ]
 
                 for chunk_idx, page_chunk in enumerate(pages):
                     page_chunk.metadata.update({
@@ -81,6 +94,9 @@ class VectorDatabaseManager:
                         "total_chunks": total_of_chunks,
                         "last_modified": datetime.now().isoformat()
                     })
+                    for metadata in metadata_to_delete:
+                        page_chunk.metadata.pop(metadata, None)
+
                     page_chunk.id = str(uuid.uuid4())
                 
                 self.vectorstore.add_documents(
@@ -90,6 +106,8 @@ class VectorDatabaseManager:
             except Exception as e:
                 print(f"Erro processando {filename}: {str(e)}")
                 continue
+        
+        return self
     
     def query(
         self,
@@ -97,7 +115,7 @@ class VectorDatabaseManager:
         uploader: Optional[List[str]] = None,
         source: Optional[List[str]] = None,
         top_k: Optional[int] = 10
-    ) -> List[Document]:
+    ) -> List[Dict[str, Any]]:
         
         """
         Consulta flexível em todos os artigos da base de dados,
@@ -130,15 +148,13 @@ class VectorDatabaseManager:
 
         return [self._format_output(doc) for doc in query_output]
     
-    def dump_doc_by_meta(
+    def search_doc_by_meta(
         self, 
         doc_id: Optional[str] = None,
         source: Optional[str] = None,
         uploader: Optional[str] = None,
-        page: Optional[int] = None,
-        limit_results: Optional[bool] = True,
-        full_dump: Optional[bool] = False
-    ) -> List[Document]:
+        metadata_only: Optional[bool] = True
+    ) -> List[Dict[str, Any]]:
         """
         Método para obter as chunks de um artigo específico, identificado pelo nome(source).
         Se o parâmetro page for None, retorna todos os chunks (documento completo).
@@ -152,25 +168,23 @@ class VectorDatabaseManager:
             {filter_argument: spec} for filter_argument, spec in (
                 ("doc_id", {"$eq": doc_id}),
                 ("source", {"$eq": source}),
-                ("uploader", {"$eq": uploader}),
-                ("page", {"$eq": page})
+                ("uploader", {"$eq": uploader})
             )if spec['$eq'] is not None
         ]
-
+        
         where_filters = {"$and": filters} if len(filters) > 1 else filters[0]
-
-        if full_dump:
-            include_fields = ["metadatas", "documents"]
-        else:
-            include_fields = ["metadatas"]
-
-        dump_result = self.vectorstore._collection.get(
+        search_result = self.vectorstore.get(
             where=where_filters,
-            include=include_fields,
-            limit=1 if limit_results else None
+            include=["metadatas"] if metadata_only else ["metadatas", "documents"]
         )
 
-        return dump_result
+        if search_result['ids'] == []:
+            return f"Artigo não encontrado. Dados da busca:\ndoc_id: {doc_id}\nsource: {source}\nuploader: {uploader}."
+        
+        return (
+            self._format_meta_search_output(search_result)
+            if metadata_only else self._format_full_doc_search_output(search_result)
+        )
 
     # ------------------------ Private Methods ------------------------
 
@@ -193,3 +207,45 @@ class VectorDatabaseManager:
             "total_chunks": doc.metadata.get("total_chunks"),
         }
 
+    @staticmethod
+    def _format_meta_search_output(only_metadatas) -> List[Dict[str, Any]]:
+        """Formatar a saída da consulta para uma lista de metadados únicos"""
+        metadatas = only_metadatas['metadatas']
+        keys_to_search = ("doc_id", "source", "total_chunks", "total_pages", "uploader")
+        unique_ocorrences = {
+            tuple(metadata[key] for key in keys_to_search)
+            for metadata in metadatas
+        }
+
+        unique_metadata = []
+        for metadata_value in unique_ocorrences:
+            unique_metadata.append(
+                {
+                    key: value for key, value in zip(keys_to_search, metadata_value)
+                }
+            )
+        
+        return unique_metadata
+    
+    @staticmethod
+    def _format_full_doc_search_output(metadata_and_chunks) -> Dict[str, List[Dict[str, Any]]]:
+        """
+        Reformata o dicionário resulatante da busca para a estrutura:
+            articles = {source: [{content:"...", chunk_id:...},{...},...{...}]}
+        """
+        chunks = metadata_and_chunks['documents']
+        metadatas = metadata_and_chunks['metadatas']
+        # Se aproveita da natureza sequencial que os chunks aparecem(grandes blocos do mesmo artigo)
+        # para organizar os chunks em um dicionário.
+        # O dicionário tem como chave a source do documento e como valor uma lista de chunks e sua metadata
+        articles = {}
+        for idx in range(len(chunks)):
+            content_and_metadata = {
+                "content": chunks[idx],
+                **metadatas[idx]
+            }
+
+            articles.setdefault(metadatas[idx]['source'], []).append(content_and_metadata)
+
+        return articles
+    
