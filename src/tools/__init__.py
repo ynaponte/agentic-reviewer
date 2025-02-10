97153230f6ba328356a169ef41b8d4638515c9a6
Ynã Ponte <ynaqp@alu.ufc.br>
2025-02-10 09:21:34 -0300

    Atualizado arquivo "__init__.py" do módulo tools

---
+++ src/tools/__init__.py
@@ -0,0 +1,5 @@
+from .query_articles_tool import QueryArticlesTool
+from .fetch_articles_tool import FetchArticlesTool
+from .fetch_metadata_tool import FetchMetadataTool
+
+__all__ = ["QueryArticlesTool", "FetchArticlesTool", "FetchMetadataTool"]
