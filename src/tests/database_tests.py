aaad83d289b0a40a4c3b22a159e5d63bdad374e4
Ynã Ponte <ynaqp@alu.ufc.br>
2025-03-10 12:44:36 -0300

    Adicionado novos testes

--- src/tests/database_tests.py
+++ src/tests/database_tests.py
@@ -1,4 +1,8 @@
 from ..utils import VectorDatabaseManager
+import multiprocessing
+
+multiprocessing.freeze_support()
+
 
 def main():
     teste_db = VectorDatabaseManager()
@@ -7,15 +11,15 @@ def main():
         .initialize_db(persist_directory='./vectorstore_test', collection_name='nova_collection')
     )
 
-    teste_db.store_documents(directory_list=['./artigos'], uploader='Yna')
+    teste_db.store_documents(directory_list=['./drafts'], uploader='Yna')
 
     # Uncomment and modify the following lines as needed for testing
     query = teste_db.query('Processamento de dados ópticos')
-    # results = teste_db.search_doc_by_meta(source='Obtencao-de-portas-logicas-com-acoplador-de-fibra-optica.pdf', uploader='Yna')
+    results = teste_db.search_doc_by_meta(source='resultado2.pdf', uploader='Yna')
     # results2 = teste_db.search_doc_by_meta(uploader='Yna', metadata_only=True)
     # results3 = teste_db.search_doc_by_meta(uploader='Yna', metadata_only=False)
     print(query)
-    # print(results)
+    print(results)
     # print(results2)
     # print(results3)
 
