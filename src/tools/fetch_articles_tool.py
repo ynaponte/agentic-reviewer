e11b7cc2a8b63eed03246a3b3e31b2cf91ce1f2a
Ynã Ponte <ynaqp@alu.ufc.br>
2025-03-13 17:31:31 -0300

    Melhoria na descrição da tool e seus parâmetros, além de garantir que todos os parâmtros são enviados pelo agente

--- src/tools/fetch_articles_tool.py
+++ src/tools/fetch_articles_tool.py
@@ -6,26 +6,26 @@ import json
 
 
 class FetchArticlesToolInput(BaseModel):    
-    source: Optional[str] = Field(
-        None, description=(
+    source: str = Field(
+        ..., description=(
             "The document's filename to search in the database and fetch its content."
             "It is case sensitive."
         )
     )
 
-    type: Optional[Literal['draft', 'reference']] = Field(
-        None, description=(
-            "Type of document to be searched for. Can be draft or reference"
-            "Can be used either to fetch all articles of such type"
-            "(if all other parameters are not specified) or to narrow"
-            "the search to only documents of such type."
+    doc_type: Literal['draft', 'reference'] = Field(
+        ..., description=(
+            "Type of document to be searched for. Can be draft or reference. "
+            "Can be used to fetch all articles of the passed type, "
+            "narrowing down the search to only documents of such type."
         )
     )
 
-    chunk_id: Optional[List[int]] = Field(
-        None, description=(
-            "A list of integers, where each corresponds to the id of a chunk,"
-            "to fetch all chunks that have ids those ids from the document."
+    chunk_id: List[int] = Field(
+        ..., description=(
+            "A list of integers, where each corresponds to the id of a chunk. "
+            "The tool will fetch all of the document's chunks that have a "
+            "corresponding id to those on the list."
         )
     )
 
@@ -33,14 +33,15 @@ class FetchArticlesToolInput(BaseModel):
 class FetchArticlesTool(BaseTool):
     name: str = "FetchArticlesTool"
     description: str = (
-        "This tool returns the content of an article as a json string containing as keys the document's"
-        "name and as value a list of it's chunks as dictionaries that have information about their"
-        "content and metadata(for more contextualization)."
-        "It is able to fetch all of an articles' chunks from the database, that match the given source and/or type"
-        "(if specified), if the 'chunk_id' parameter is omitted, otherwise, it fetches data from all chunks specified"
-        "in 'chunk_id'. This tool must be used when the content of a specific source is need, may that be all of it or chunk by chunk."
-        "If can't find any documents with requested inputs, the tool will give the message 'Artigo não encontrado. "
-        "Dados da busca:' followed by the inputs provided to the tool"
+        "This tool returns the content of a document, sliced in chunks, with addition to metadata such as the total of chunks and total of "
+        "pages the document was sliced into and has, respectivally. This information is return as a JSON type string, where the document "
+        "is identified by it's name (for example, 'Document1.pdf') at the root of the JSON object.Within this JSON object, the document's "
+        "content is segmented into chunks labeled sequentially (such as 'Chunk 1', 'Chunk 2', etc.), and each chunk contains a 'content' "
+        "field with the corresponding portion of text. Additionally, the JSON includes a metadata section at the end that specifies the "
+        "total number of chunks and the total number of pages of the document. The chunks returned will vary accordingly to the requested "
+        "ids and may be based on any list of integers, as long as the corresponding chunks exist."
+        "If can't find anything with the requested inputs, a warning will be triggered. "
+        "All inputs must be passed for proper functioning."
     )
 
     args_schema: Type[BaseModel] = FetchArticlesToolInput
@@ -52,17 +53,27 @@ class FetchArticlesTool(BaseTool):
 
     def _run(
         self,
-        source: Optional[str] = None,
-        type: Optional[str] = None,
-        chunk_id: Optional[List[int]] = None
-    ) -> List[Dict[str, Any]]:
-        return json.dumps(
-            self._vectordatabase.search_doc_by_meta(
+        source: str,
+        doc_type: str,
+        chunk_id: List[int]
+    ) -> str:
+        try:
+            search_result = self._vectordatabase.search_doc_by_meta(
                 source=source, 
-                type=type,
+                type=doc_type,
                 metadata_only=False,
                 chunk_id=chunk_id
-            ),
-            indent=4
-        )
+            )
+            if search_result == []:
+                return json.dumps(
+                    {"[WARNING]": "No document found. Please, ensure inputs are correct"}
+                )
+            return json.dumps(
+                search_result,
+                indent=4
+            )
+        except AttributeError:
+            return json.dumps(
+                {"[ERROR]": "Incorrect usage of the tool. All inputs must be passed."}
+            )
     
