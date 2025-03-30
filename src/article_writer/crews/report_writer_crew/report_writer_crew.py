96bf4290f49ddad97bb840e1ffbdc552975c8e1b
Ynã Ponte <ynaqp@alu.ufc.br>
2025-03-30 12:32:45 -0300

    Adicionado a equipe responsável por juntar as analises dos lotes em um único relatório.

---
+++ src/article_writer/crews/report_writer_crew/report_writer_crew.py
@@ -0,0 +1,79 @@
+from crewai import Agent, Crew, Task, Process
+from crewai.project import CrewBase, agent, crew, task
+from crewai.llm import LLM
+
+
+@CrewBase
+class ReportConsolidation:
+
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+    consolidation_llm = LLM(
+        model="ollama/llama3.1:latest",
+        base_url="http://localhost:11434",
+        max_completion_tokens=8000,
+        max_tokens=128000,
+        temperature=0.5
+    )
+    writer_llm = LLM(
+        model="ollama/llama3.1:latest",
+        base_url="http://localhost:11434",
+        max_completion_tokens=8000,
+        max_tokens=128000,
+        temperature=0.6
+    )
+    
+    @agent
+    def report_consolidator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['report_consolidator'],
+            llm=self.consolidation_llm
+        )
+    
+    @agent
+    def report_writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['report_writer'],
+            llm=self.writer_llm
+        )
+    
+    @task
+    def final_critical_consolidation (self) -> Task:
+        return Task(
+            config=self.tasks_config['final_critical_consolidation']
+        )
+    
+    @task
+    def final_key_points_consolidation(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_key_points_consolidation']
+        )
+    
+    @task
+    def final_methodology_consolidation(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_methodology_consolidation']
+        )
+    
+    @task
+    def final_results_consolidation(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_results_consolidation']
+        )
+    
+    @task
+    def final_report_consolidation(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_report_consolidation']
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        crew = Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True
+        )
+        return crew
+    
