51f85660c8510b2e28ea5f213be131066c57dc4b
ynaponte <ynaqp@alu.ufc.br>
2025-04-03 15:56:13 -0300

    Versão final de Chunk Report

--- src/article_writer/crews/doc_chunk_review_crew/chunk_review_crew.py
+++ src/article_writer/crews/doc_chunk_review_crew/chunk_review_crew.py
@@ -1,7 +1,6 @@
 from crewai import Agent, Crew, Task, Process
 from crewai.project import CrewBase, agent, crew, task
 from crewai.llm import LLM
-from src.tools import FetchArticlesTool
 
 
 @CrewBase
@@ -11,9 +10,9 @@ class ChunkReviewCrew:
     agents_config = "config/agents.yaml"
     tasks_config = "config/tasks.yaml"
     std_llm = LLM(
-        model="ollama/llama3.1:latest",
+        model="ollama/qwen2.5:14b",
         base_url="http://localhost:11434",
-        max_completion_tokens=8000,
+        max_completion_tokens=128000,
         max_tokens=128000,
         temperature=0.5
     )
@@ -24,29 +23,23 @@ class ChunkReviewCrew:
             config=self.agents_config['critical_analyst'],
             llm=self.std_llm,
         )
-    
-    @task
-    def critical_analysis(self) -> Task:
-        return Task(
-            config=self.tasks_config['critical_analysis'],
+    @agent
+    def methodology_evaluator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['methodology_evaluator'],
+            llm=self.std_llm,
         )
     
     @task
-    def key_points_extraction(self) -> Task:
+    def methodology_results_consistency(self) -> Task:
         return Task(
-            config=self.tasks_config['key_points_extraction']
+            config=self.tasks_config['methodology_results_consistency']
         )
     
     @task
-    def methodology_analysis(self) -> Task:
+    def results_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['methodology_analysis']
-        )
-    
-    @task
-    def results_analysis(self) ->Task:
-        return Task(
-            config=self.tasks_config['results_analysis']
+            config=self.tasks_config['results_analysis'],
         )
     
     @crew
