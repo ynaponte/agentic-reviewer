26d18c56fa8565e2869cc868b008810fe6942eeb
ynaponte <ynaqp@alu.ufc.br>
2025-04-08 16:22:21 -0300

    change: Voltou-se a processamento paralelo dos batchs add: variáveis de estado para armazenar os capítulos escritos; geração de outlines; geração do capítulo.

--- src/article_writer/main.py
+++ src/article_writer/main.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 from crewai.flow import Flow, listen, start
 from .crews.doc_chunk_review_crew import ChunkReviewCrew
 from .crews.report_writer_crew.report_writer_crew import ReportWriterCrew
+from .crews.outline_crew.outline_crew import OutlineCrew
+from .crews.res_disc_writer.res_dics_writer_crew import ResultAndDiscussionCrew
 from ..tools import FetchArticlesTool
 from typing import Dict, List 
 from ..utils import VectorDatabaseManager
@@ -25,8 +27,11 @@ class ArticleWriterState(BaseModel):
         "em cristal fotônico"
     )
     drafts_documents: List[str] = ['Resultado1.pdf']
-    docs_chunks_reports: Dict[str, Dict[str, List]] = {}
-    docs_reports: List[str] = []
+    full_analysis: str = ""
+    technical_elements: str = ""
+    results_discussion_outline: str = ""
+    conclusion_outline: str = ""
+    methodology_outline: str = ""
     discussion: str = ""
     conclusion: str = ""
     #chunk_reports: List[AnaliseCriticaResultadosDiscussao] = []
@@ -47,87 +52,94 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
     @listen(start_flow)
     async def doc_process_by_chunks(self):
         def batching(num_chunks, batch_size):
-            '''
-            Defina o tamanho do lote de acordo?
-                - Llama3.1:8b -> 
-            :param: batch_size - tamanho do lote de chunks
-            '''
             return [
                 list(range(i, min(i + batch_size, num_chunks))) 
                 for i in range(0, num_chunks, batch_size)
             ]
         
-        def process_single_document(
+        async def process_single_document(
             doc_name: str, 
             chunk_indexes: List[int], 
             batch_index: int,
             content: str
         ):
-            output = ChunkReviewCrew().crew().kickoff(
+            output = await ChunkReviewCrew().crew().kickoff_async(
                     inputs={
                         "target_document": doc_name,
                         "chunk_indexes": chunk_indexes,
-                        "theme": self.state.theme,
                         "batch_number": batch_index,
                         "content": content         
                     }
                 )
             return output
 
-        tasks = []
+        fragmented_analysis = {}   # Dicionário responsável por armazenar os fragmentos de analises
         for draft_document in self.state.drafts_documents:
+            tasks = []   # Inicializa a lista para as tarefas assíncronas
             doc_meta = self.articles_db.search_doc_by_meta(source=draft_document, type='draft')
-            chunk_batches = batching(doc_meta[0]['total_chunks'], 2)
+            chunk_batches = batching(doc_meta[0]['total_chunks'], 3)
             for i, batch in enumerate(chunk_batches, start=1):
                 content = FetchArticlesTool()._run(source=draft_document, chunk_id=batch, doc_type='draft')
-                #task = asyncio.create_task(process_single_document(draft_document, batch, i))
-                #tasks.append(task)
-                tasks.append(process_single_document(draft_document, batch, i, content))
+                tasks.append(asyncio.create_task(process_single_document(draft_document, batch, i, content)))
 
-            #crews_outputs = await asyncio.gather(*tasks)
-            crews_outputs = tasks
-            chunk_report = {}
-            # Armazena os resultados de cada chunk
+            crews_outputs = await asyncio.gather(*tasks)
+            # Separação e armazenamento dos fragmentos de analises
             for crew_output in crews_outputs:
-                # Indice em crew_output.tasks_output corresponte a task:
-                #   0 -> 'critical_analysis'
-                #   1 -> 'key_points_extraction'
-                #   2 -> 'methodology_analysis'
-                #   3 -> 'results_analysis'
-                #   4 -> 'elements_extraction'
-                #   5 -> 'report_consolidation'
-                chunk_report.setdefault("critical_analysis", []).append(
+                fragmented_analysis.setdefault("methodology_results_consistency", []).append(
                     crew_output.tasks_output[0].raw
                 )
-                chunk_report.setdefault("key_points_extraction", []).append(
+                fragmented_analysis.setdefault("results_analysis", []).append(
                     crew_output.tasks_output[1].raw
                 )
-                chunk_report.setdefault("methodology_analysis", []).append(
-                    crew_output.tasks_output[2].raw
-                )
-                chunk_report.setdefault("results_analysis", []).append(
-                    crew_output.tasks_output[3].raw
-                )
-
-            self.state.docs_chunks_reports[draft_document] = chunk_report
-
-    @listen(doc_process_by_chunks)
-    def report_consolidation(self):
-        for rprt_frag in self.state.docs_chunks_reports:
-            report = ReportWriterCrew.crew().kickoff(
-                inputs={
-                    "critical_analysis_fragments": rprt_frag["critical_analysis"],
-                    "key_points_fragments": rprt_frag["key_points_extraction"],
-                    "methodology_fragments": rprt_frag["methodology_analysis"],
-                    "results_fragments": rprt_frag["results_analysis"],
+                self.state.technical_elements += crew_output.tasks_output[2].raw
+                
+        full_analysis = ""
+        for frags in fragmented_analysis.values():
+            output = ReportWriterCrew().crew().kickoff(
+            inputs={
+                "input_text": "".join(
+                        frag for frag in frags
+                    ),
                 }
             )
+            full_analysis += output.raw
 
-            self.state.docs_reports.append(report.raw)
+        full_analysis = ReportWriterCrew().crew().kickoff(
+            inputs={
+                "input_text": full_analysis
+            }
+        )
+        self.state.full_analysis = full_analysis.raw
 
-    @listen(report_consolidation)
-    def res_and_disc_sec_writting(self):
-        pass
+    @listen(doc_process_by_chunks)
+    def outline_generation(self):
+        asd = self.state.full_analysis
+        outcrew_output = OutlineCrew().crew().kickoff(
+            inputs={
+                "analysis": self.state.full_analysis,
+                "theme": self.state.theme
+                }
+        )
+        for task_output in outcrew_output.tasks_output:
+            if task_output.name == "generate_results_discussion_outline":
+                self.state.results_discussion_outline = task_output.raw
+                print(f"Outline de Resultados e Discussao:\n\n{task_output.raw}")
+            elif task_output.name == "generate_conclusion_outline":
+                self.state.conclusion_outline = task_output.raw
+                print(f"Outline de conclusão:\n\n{task_output.raw}")
+            else:
+                self.state.methodology_outline = task_output.raw
+                print(f"Outline de metodologia:\n\n{task_output.raw}")
+
+    @listen(outline_generation)
+    def res_and_disc_chapter_generation(self):
+        chapter = ResultAndDiscussionCrew().crew().kickoff(
+            inputs={
+                "chapter_outline": self.state.results_discussion_outline,
+                "theme": self.state.theme
+            }
+        )
+        print(chapter.raw)
 
 def kickoff():
     article_flow = ArticleWriterFlow()
