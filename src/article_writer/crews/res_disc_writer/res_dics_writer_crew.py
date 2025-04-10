b96225cd1b71c4ff4fff104e5d605afa333b2487
ynaponte <ynaqp@alu.ufc.br>
2025-04-10 16:46:46 -0300

    MOD: Aumento da temperatura da LLM do escritor e inclusão (ainda a terminar) de novas tarefas para melhoria da geração de texto.

--- src/article_writer/crews/res_disc_writer/res_dics_writer_crew.py
+++ src/article_writer/crews/res_disc_writer/res_dics_writer_crew.py
@@ -9,14 +9,21 @@ class ResultAndDiscussionCrew():
 
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
-    llm = LLM(
+    writer_llm = LLM(
         model="ollama/qwen2.5:14b-instruct-q8_0",
         base_url="http://localhost:11434",
 		max_tokens=128000,
-		temperature=0.3
+		temperature=0.7
     )
 
     researcher_llm = LLM(
+        model='ollama/qwen2.5:14b',
+        base_url='http://localhost:11434',
+        max_tokens=128000,
+        temperature=0.2
+    )
+
+    tool_call_llm = LLM(
         model='ollama/qwen2.5:14b',
         base_url='http://localhost:11434',
         max_tokens=128000,
@@ -29,26 +36,27 @@ class ResultAndDiscussionCrew():
             config=self.agents_config['research_agent'],
             llm=self.researcher_llm,
             tools=[QueryArticlesTool()]
-        )
 
     @agent
     def results_discussion_writer(self) -> Agent:
         return Agent(
             config=self.agents_config['results_discussion_writer'],
-            llm=self.llm,
-        )
-    
-    @task
-    def research_outline_topics(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_outline_topics'],
+            llm=self.writer_llm,
+            function_calling_llm=self.tool_call_llm,
             tools=[QueryArticlesTool()]
+        )
+
+    @task
+    def select_complementary_elements(self) -> Task:
+        return Task(
+            config=self.tasks_config['select_complementary_elements'],
         )
     
     @task
     def results_discussion_writting(self) -> Task:
         return Task(
-            config=self.tasks_config['results_discussion_writting']
+            config=self.tasks_config['results_discussion_writting'],
+            tools=[QueryArticlesTool()]
         )
     
     @crew
