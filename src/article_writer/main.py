4dc2dfbbf432b8225aa0b000495b1f91c3f7b71d
ynaponte <ynaqp@alu.ufc.br>
2025-04-14 16:23:46 -0300

    reestruturação para nova forma de processar a partir do relatório pronto

--- src/article_writer/main.py
+++ src/article_writer/main.py
@@ -50,77 +50,15 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
         )
 
     @listen(start_flow)
-    async def draft_processament(self):
-        def batch_generator(num_chunks, batch_size):
-            return [
-                list(range(i, min(i + batch_size, num_chunks))) 
-                for i in range(0, num_chunks, batch_size)
-            ]
-        
-        async def process_single_batch(
-            doc_name: str, 
-            chunk_indexes: List[int], 
-            batch_index: int,
-            content: str
-        ):
-            output = await ChunkReviewCrew().crew().kickoff_async(
-                    inputs={
-                        "target_document": doc_name,
-                        "chunk_indexes": chunk_indexes,
-                        "batch_number": batch_index,
-                        "content": content         
-                    }
-                )
-            return output
-
-        fragmented_analysis = {}   # Dicionário responsável por armazenar os fragmentos de analises
-        for draft_document in self.state.drafts_documents:
-            tasks = []   # Inicializa a lista para as tarefas assíncronas
-            doc_meta = self.articles_db.search_doc_by_meta(source=draft_document, type='draft')
-            chunk_batches = batch_generator(doc_meta[0]['total_chunks'], 3)
-            for i, batch in enumerate(chunk_batches, start=1):
-                content = FetchArticlesTool()._run(source=draft_document, chunk_id=batch, doc_type='draft')
-                tasks.append(asyncio.create_task(process_single_batch(draft_document, batch, i, content)))
-
-            crews_outputs = await asyncio.gather(*tasks)
-            # Separação e armazenamento dos fragmentos de analises
-            for crew_output in crews_outputs:
-                fragmented_analysis.setdefault("methodology_results_consistency", []).append(
-                    crew_output.tasks_output[0].raw
-                )
-                fragmented_analysis.setdefault("results_analysis", []).append(
-                    crew_output.tasks_output[1].raw
-                )
-                self.state.technical_elements += crew_output.tasks_output[2].raw
-                
-        full_analysis = ""
-        for frags in fragmented_analysis.values():
-            output = ReportWriterCrew().crew().kickoff(
-            inputs={
-                "input_text": "".join(
-                        frag for frag in frags
-                    ),
-                }
-            )
-            full_analysis += output.raw
-
-        full_analysis = ReportWriterCrew().crew().kickoff(
-            inputs={
-                "input_text": full_analysis
-            }
-        )
-        self.state.full_analysis = full_analysis.raw
-
-    @listen(draft_processament)
-    def outline_generation(self):
+    def generate_outlines(self):
+        draft_report = self.articles_db.search_doc_by_meta(source='Relatorio.pdf', metadata_only=False)
         outcrew_output = OutlineCrew().crew().kickoff(
             inputs={
-                "analysis": self.state.full_analysis,
-                "theme": self.state.theme
-                }
+                "report": draft_report['Relatorio.pdf']['text_content'],
+            }
         )
         for task_output in outcrew_output.tasks_output:
-            if task_output.name == "generate_results_discussion_outline":
+            if task_output.name == "generate_outline_results_discussion":
                 self.state.results_discussion_outline = task_output.raw
                 print(f"Outline de Resultados e Discussao:\n\n{task_output.raw}")
             elif task_output.name == "generate_conclusion_outline":
@@ -130,7 +68,7 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
                 self.state.methodology_outline = task_output.raw
                 print(f"Outline de metodologia:\n\n{task_output.raw}")
 
-    @listen(outline_generation)
+    @listen(generate_outlines)
     def res_and_disc_chapter_generation(self):
         chapter = ResultAndDiscussionCrew().crew().kickoff(
             inputs={
