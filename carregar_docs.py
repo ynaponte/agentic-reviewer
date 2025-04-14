8a928e34f7cc41389ff3b7787290a155623aadb4
ynaponte <ynaqp@alu.ufc.br>
2025-04-14 14:24:23 -0300

    ADD: Inclusão da pasta "relatorio" contendo o PDF do relatório dos drafts

--- carregar_docs.py
+++ carregar_docs.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
         persist_directory="./article_vectorstore",
         collection_name="flow_test_collection"
     )
-    articles_db.store_documents(directory_list=['./drafts'], type='draft')
-    articles_db.store_documents(directory_list=['./artigos'], type='reference')
+    articles_db.store_documents(directory_list=['./drafts'], doc_type='draft')
+    articles_db.store_documents(directory_list=['./artigos'], doc_type='reference')
+    articles_db.store_documents(directory_list=['./relatorio'], doc_type='report')
 
     print(articles_db.search_doc_by_meta(source='Resultado1.pdf', type='draft')) 
