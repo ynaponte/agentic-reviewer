2f9e70df237d3ad04a159fbd52a813a3fd6485db
ynaponte <ynaqp@alu.ufc.br>
2025-04-22 17:16:02 -0300

    Dor e ódio

--- src/article_writer/crews/outline_crew/outline_crew.py
+++ src/article_writer/crews/outline_crew/outline_crew.py
@@ -2,6 +2,49 @@ from crewai import Agent, Crew, Task, Process
 from crewai.project import CrewBase, agent, crew, task
 from crewai.llm import LLM
 from src.article_writer.types.results_report import ElementsList
+from pydantic import BaseModel, Field
+from typing import Literal, List, Optional, Union
+
+
+class VisualElements(BaseModel):
+    name: str = Field(description="Name of the visual element")
+    description: str = Field(description="Description of the visual element")    
+
+class DiscussionTopic(BaseModel):
+    topic: str = Field(description="Description of the topic to be discussed")
+    visual_elements_to_contextualize: Optional[List[VisualElements]] = Field(
+        default=[],
+        description="Visual elements that are important to the topic and should be contextualized in the section's texts"
+    )
+    numerical_results: Optional[List[str]] = Field(
+        default=[],
+        description="Numerical results that are important to the topic"
+    )
+
+class ChapterSection(BaseModel):
+    section_title: str = Field(
+        description="Title of the section"
+    )
+    level: int = Field(
+        description=(
+            "An integer refering to the hierarchical level of the section in the chapter."
+            "The number 1 indicates a section, 2 a subsection, 3 a subsubsection and 4 a subsubsubsection."
+        )
+    )
+    children: Optional[List[str]] = Field(
+        default=[],
+        description="A list with the titles of every section that's a subsection of the current one."
+    )
+    discution_topics: List[DiscussionTopic] = Field(
+        description="List of topics that the text content of the chapter should discuss"
+    )
+
+
+class Outline(BaseModel):
+    chapter_name: str = Field(description="Name of the chapter")
+    sections: List[ChapterSection] = Field(
+        description="Sections, subsections, subsubsections, etc, that the chapter contains."
+    )
 
 
 @CrewBase
@@ -27,19 +70,22 @@ class OutlineCrew:
     @task
     def generate_outline_results_discussion(self) -> Task:
         return Task(
-            config=self.tasks_config['generate_outline_results_discussion']
+            config=self.tasks_config['generate_outline_results_discussion'],
+            output_json=Outline
         )
     
     @task
     def generate_outline_conclusion(self) -> Task:
         return Task(
-            config=self.tasks_config['generate_outline_conclusion']
+            config=self.tasks_config['generate_outline_conclusion'],
+            output_json=Outline
         )
     
     @task
     def generate_outline_methodology(self) -> Task:
         return Task(
-            config=self.tasks_config['generate_outline_methodology']
+            config=self.tasks_config['generate_outline_methodology'],
+            output_json=Outline
         )
     
     @crew
--- src/article_writer/crews/technical_chapter_writer/technical_chapter_writer_crew.py
+++ src/article_writer/crews/technical_chapter_writer/technical_chapter_writer_crew.py
@@ -11,7 +11,7 @@ class TechnicalChapterWriterCrew():
     tasks_config = 'config/tasks.yaml'
 
     manager_llm = LLM(
-        model="ollama/deepseek-r1:7b",
+        model="ollama/deepseek-r1:32b",
         base_url="http://localhost:11434",
         timeout=1800.0,
         max_tokens=32000,
@@ -20,7 +20,7 @@ class TechnicalChapterWriterCrew():
     )
 
     researcher_and_editor_llm = LLM(
-        model="ollama/qwen2.5:3b-instruct-q6_K",
+        model="ollama/qwen2.5:32b",
         base_url="http://localhost:11434",
         timeout=1800.0,
         max_tokens=32000,
@@ -28,7 +28,7 @@ class TechnicalChapterWriterCrew():
     )
 
     func_caller = LLM(
-        model="ollama/qwen2.5:3b-instruct-q6_K",
+        model="ollama/qwen2.5:32b",
         base_url="http://localhost:11434",
         timeout=1800.0,
         max_tokens=32000,
@@ -36,12 +36,21 @@ class TechnicalChapterWriterCrew():
     )
 
     writer_llm = LLM(
-        model="ollama/qwen2.5:3b-instruct-q6_K",
+        model="ollama/qwen2.5:32b",
         base_url="http://localhost:11434",
         timeout=1800.0,
         max_tokens=32000,
         temperature=0.7
     )
+
+    @agent
+    def chapter_manager(self) -> Agent:
+        return Agent(
+            config=self.agents_config['chapter_manager'],
+            allow_delegation=True,
+            verbose=True,
+            llm=self.manager_llm
+        )
 
     @agent
     def topic_researcher(self) -> Agent:
@@ -74,17 +83,29 @@ class TechnicalChapterWriterCrew():
             config=self.tasks_config['write_technical_chapter'],
         )
 
-    #@task
-    #def assemble_and_finalize_chapter(self) -> Task:
-    #    return Task(
-    #        config=self.tasks_config['assemble_and_finalize_chapter'],
-    #    )
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'],
+            tools=[QueryArticlesTool()]
+        )
+    
+    @task
+    def write_section(self) -> Task:
+        return Task(
+            config=self.tasks_config['write_section'],
+        )
+    
+    @task
+    def final_editing(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_editing'],
+        )
 
     @crew
     def crew(self) -> Crew:
         return Crew(
-            #manager_agent=self.chapter_manager(),
-            manager_llm=self.manager_llm,
+            manager_agent=self.chapter_manager(),
             agents=[
                 self.topic_researcher(),
                 self.technical_writer(),
@@ -92,10 +113,12 @@ class TechnicalChapterWriterCrew():
             ],
             tasks=[
                 self.write_technical_chapter(),
-                #self.assemble_and_finalize_chapter()
+                self.research_task(),
+                self.write_section(),
+                self.final_editing()
             ],
             process=Process.hierarchical,
             verbose=True,
-            planning=False,
-            #planning_llm=self.planner
+            planning=True,
+            planning_llm=self.manager_llm
         )
