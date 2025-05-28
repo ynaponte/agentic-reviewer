689d2418e4609c22439b9d585fb0743376fe3d2a
Ynã Ponte <ynaqp@alu.ufc.br>
2025-05-28 14:03:59 -0300

    Adequação do main para novas equipes

--- src/article_writer/main.py
+++ src/article_writer/main.py
@@ -1,38 +1,23 @@
 from pydantic import BaseModel
 from crewai.flow import Flow, listen, start, and_
-from .crews.doc_chunk_review_crew import ChunkReviewCrew
-from .crews.report_writer_crew.report_writer_crew import ReportWriterCrew
 from .crews.res_and_disc_crew.r_d_outline_crew.r_d_outline_crew import RDOutlineCrew
 from .crews.res_and_disc_crew.r_d_topic_rag_crew.r_d_topic_rag_crew import RDTopicRagCrew
 from .crews.conclusion_crew.c_outline_crew.c_outline_crew import COutlineCrew
 from .crews.conclusion_crew.c_topic_rag_crew.c_topic_rag_crew import ConclusionTopicRagCrew
-from pydantic import BaseModel  
-from ..tools import QueryArticlesTool
+from .crews.methodology_crew.methodology_outline_crew.methodology_outline_crew import MethodologyOutlineCrew
+from .crews.methodology_crew.methodology_topic_rag_crew.methodology_topic_rag_crew import MethodologyTopicRagCrew
+from .crews.theoretical_fdmt_crew.theoretical_fdmt_outline_crew.theoretical_fdmt_outline_crew import TheoreticalFdmtOutlineCrew
+from .crews.theoretical_fdmt_crew.theoretical_fdmt_rag_crew.theoretical_fdmt_topic_rag_crew import TheoreticalFdmtTopicRagCrew
 from typing import Dict, List
 from ..utils import VectorDatabaseManager
 # from .types.doc_report import AnaliseCriticaResultadosDiscussao
-from crewai import Agent, Crew, Process, Task
-from crewai.llm import LLM
 import asyncio
-import itertools
 import json
-
-
-class ChunkReview(BaseModel):
-    """A class representing a chunk review."""
-    critical_analysis: List[str]
-    key_points_detailing: List[str]
-    methodology_analysis: List[str]
-    techenical_elements: List[str]
 
 
 class ArticleWriterState(BaseModel):
     draft_report: str = ""
-    results_discussion_section: dict = {}
-    conclusion_outline: str = ""
-    methodology_outline: str = ""
-    discussion: str = ""
-    conclusion: str = ""
+    sections_and_content: List[Dict[str, str]] = []
     # chunk_reports: List[AnaliseCriticaResultadosDiscussao] = []
 
 
@@ -75,7 +60,7 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
       res_and_disc_outline = self.res_and_disc_outline_generation()
     return res_and_disc_outline
   
-  #@listen(res_and_disc_outline_generation)
+  @listen(dev_res_and_disc_outline_generation)
   async def res_and_disc_chapter_generation(self, results_discussion_outline):
     # Função para permitir chamada assincrona da crew de escrita dos tópicos
     async def acall_write_topic_crew(
@@ -103,8 +88,11 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
       return topic_content
 
     subs_outlines = results_discussion_outline.get('subsections', [])
-    visual_elements_to_retrive = {}
-    subsections_content = {}
+    # Inicializa o dicionário para armazenar o conteúdo gerado para a seção e suas subseções
+    results_and_discussion_section = {
+      "section_name": "Resultado e Discussão", 
+      "topics": []
+    }
     for subsection in subs_outlines:
       async_tasks_to_exec = []
       for disc_topic in subsection['discussion_topics']:
@@ -115,10 +103,6 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
           )
           for element in disc_topic.get('visual_elements', [])
         ])
-        if subsection['subsection_name'] not in visual_elements_to_retrive:
-          visual_elements_to_retrive[subsection['subsection_name']] = ""
-    
-        visual_elements_to_retrive[subsection['subsection_name']] += visual_elements_to_contextualize + "\n"
         numerical_results_to_include = "\n".join([
           (
               f'- verbatim value: {result['verbatim_value']}; '
@@ -131,7 +115,7 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
           asyncio.create_task(
             acall_write_topic_crew(
                 subsection['subsection_name'], 
-                disc_topic['topic'],
+                disc_topic['topic_title'],
                 disc_topic['topic_description'],
                 visual_elements_to_contextualize, 
                 numerical_results_to_include,
@@ -142,14 +126,15 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
           )
         )
       subsection_topics_content = await asyncio.gather(*async_tasks_to_exec)
-      subsections_content[subsection['subsection_name']] = [
-        topic_content.json_dict for topic_content in subsection_topics_content
-      ]
-    results_and_discussion_section = {
-      "section_name": "Resultado e Discussão", 
-      "topics_content": subsections_content
-    }
-    self.state.results_discussion = results_discussion_outline
+      for topic_content in subsection_topics_content:
+        # Prepara os resultados para serem armazenados em formato JSON, seguindo uma estrutura padrão,
+        # onde cada tópico, além da informação sobre seu nome e seu conteúdo textual, carrega também a 
+        # qual seção ele pertence. Caso seja da principal, se informa como 'null'.
+        topic_content = topic_content.json_dict 
+        topic_content['subsection'] = subsection['subsection_name']
+        results_and_discussion_section['topics'].append(topic_content)
+    
+    self.state.sections_and_content.append(results_discussion_outline)
     return results_and_discussion_section
   
   @listen(res_and_disc_chapter_generation)
@@ -163,7 +148,7 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
     return conclusion_outline
 
   # Utilizado para agilizar desenvolvimento
-  @listen(dev_res_and_disc_outline_generation)
+  #@listen(dev_res_and_disc_outline_generation)
   def dev_conclusion_outline_generation(self, results_discussion_outline):
     outline_file_path = './outlines/conclusion_outline.json'
     try:
@@ -175,7 +160,7 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
       conclusion_outline = self.conclusion_outline_generation(results_discussion_outline)
     return conclusion_outline
 
-  @listen(dev_conclusion_outline_generation)
+  @listen(conclusion_outline_generation)
   # NOTA: talvez seja bom esperar o também o termino da execução de `res_and_disc_chapter_generation`, para evitar muitas tarefas assícronas rodando.
   async def conclusion_chapter_generation(
     self,
@@ -203,20 +188,164 @@ class ArticleWriterFlow(Flow[ArticleWriterState]):
     
     discussion_topics = conclusion_outline['discussion_topics']
     async_tasks_to_exec = []
+    conclusion_section = {
+      "section_name": "Conclusão",
+      "topics": []
+    }
     for discussion_topic in discussion_topics:
       async_tasks_to_exec.append(
         asyncio.create_task(
           acall_write_topic_crew(
-            discussion_topic['topic'],
+            discussion_topic['topic_title'],
             discussion_topic['rhetorical_purpose'],
             discussion_topic['topic_description'],
             discussion_topic['narrative_guidance']
           )
         )
       )
-    conclusion_topics = await asyncio.gather(*async_tasks_to_exec)
-    return conclusion_topics
+    conclusion_topics_content = await asyncio.gather(*async_tasks_to_exec)
+    for topic_content in conclusion_topics_content:
+      topic_content = topic_content.json_dict
+      topic_content['subsection'] = 'main'
+      conclusion_section['topics'].append(topic_content)
+
+    self.state.sections_and_content.append(conclusion_outline)
+    return conclusion_section
   
+  @listen(conclusion_chapter_generation)
+  def methodology_outline_generation(self):
+    methodology_outline = MethodologyOutlineCrew().crew().kickoff(
+        inputs={
+          "report": self.state.draft_report['Relatorio.pdf']['text_content'],
+          "generated_sections_content": self.state.sections_and_content
+        }
+    )
+    return methodology_outline
+  
+  @listen(methodology_outline_generation)
+  async def methodology_chapter_generation(self, methodology_outline):
+    async def acall_write_topic_crew(
+      subsection_name,
+      subsection_description,
+      topic_title,
+      rhetorical_purpose,
+      topic_description,
+      narrative_guidance
+    ):
+      methodology_topics_content = (
+        await MethodologyTopicRagCrew()
+        .crew()
+        .kickoff_async(
+          inputs={               
+            "methodology_subsection_name": subsection_name,
+            "methodology_subsection_description": subsection_description,
+            "methodology_topic_title": topic_title,
+            "methodology_topic_description": topic_description,
+            "methodology_rhetorical_purpose": rhetorical_purpose,
+            "methodology_narrative_guidance": narrative_guidance
+          }
+        )
+      )
+      return methodology_topics_content
+    
+    methodology_section = {
+      "section_name": "Metodologia",
+      "topics": []
+    }
+    for subsecion in methodology_outline['subsections']:
+      async_tasks_to_exec = []  # Inicia/reseta a lista de tarefas
+      subsecion_name = subsecion['subsection_name']
+      subsecion_description = subsecion['subsection_description']
+      for discussion_topic in subsecion['discussion_topics']:
+        async_tasks_to_exec.append(
+          asyncio.create_task(
+            acall_write_topic_crew(
+              subsection_name=subsecion_name,
+              subsection_description=subsecion_description,
+              topic_title=discussion_topic['topic_title'],
+              topic_description=discussion_topic['topic_description'],
+              rhetorical_purpose=discussion_topic['rhetorical_purpose'],
+              narrative_guidance=discussion_topic['narrative_guidance']
+            )
+          )
+        )
+      methodology_topics_content = await asyncio.gather(*async_tasks_to_exec)
+      for topic_content in methodology_topics_content:
+        topic_content = topic_content.json_dict
+        topic_content['subsection'] = subsecion_name  # Cria uma nova chave no dicionário para armazenar a que seção o tópico pertence
+        methodology_section['topics'].append(topic_content)
+
+    self.state.sections_and_content.append(methodology_outline)
+    return methodology_section    
+  
+  @listen(and_(methodology_outline_generation, methodology_chapter_generation))
+  def theoretical_fdmt_outline_generation(self, methodology_outline, _):
+    theoretical_fdmt_outline = TheoreticalFdmtOutlineCrew().crew().kickoff(
+      inputs={
+        "report": self.state.draft_report['Relatorio.pdf']['text_content'],
+        "methodology_outline": methodology_outline
+      }
+    )
+    return theoretical_fdmt_outline
+
+  @listen(theoretical_fdmt_outline_generation)
+  async def theoretical_fdmt_chapter_generation(self, theoretical_fdmt_outline):
+    async def acall_write_topic_crew(
+      subsection_title,
+      subsection_description,
+      topic_title,
+      rhetorical_purpose,
+      topic_description,
+      narrative_guidance
+    ):
+      theoretical_fdmt_topics_content = (
+        await TheoreticalFdmtTopicRagCrew()
+        .crew()
+        .kickoff_async(
+          inputs={
+            "theoretical_foundation_subsection_name": subsection_title,
+            "theoretical_foundation_subsection_description": subsection_description,            
+            "theoretical_foundation_topic_title": topic_title,
+            "theoretical_foundation_rhetorical_purpose": rhetorical_purpose,
+            "theoretical_foundation_topic_description": topic_description,
+            "theoretical_foundation_narrative_guidance": narrative_guidance
+          }
+        )
+      )
+      return theoretical_fdmt_topics_content
+    
+    theoretical_fdmt_section = {
+      "section_name": "Fundamentação Teórica",
+      "topics": []
+    }
+
+    for subsecion in theoretical_fdmt_outline['subsections']:
+      async_tasks_to_exec = []  # Inicia/reseta a lista de tarefas
+      subsecion_name = subsecion['subsection_name']
+      subsecion_description = subsecion['subsection_description']
+      for discussion_topic in subsecion['discussion_topics']:
+        async_tasks_to_exec.append(
+          asyncio.create_task(
+            acall_write_topic_crew(
+              subsection_name=subsecion_name,
+              subsection_description=subsecion_description,
+              topic_title=discussion_topic['topic_title'],
+              topic_description=discussion_topic['topic_description'],
+              rhetorical_purpose=discussion_topic['rhetorical_purpose'],
+              narrative_guidance=discussion_topic['narrative_guidance']
+            )
+          )
+        )
+      methodology_topics_content = await asyncio.gather(*async_tasks_to_exec)
+      for topic_content in methodology_topics_content:
+        topic_content = topic_content.json_dict
+        topic_content['subsection'] = subsecion_name  # Cria uma nova chave no dicionário para armazenar a que seção o tópico pertence
+        theoretical_fdmt_section['topics'].append(topic_content)
+
+    self.state.sections_and_content.append(theoretical_fdmt_outline)
+    return theoretical_fdmt_section
+    
+
 def kickoff():
     article_flow = ArticleWriterFlow()
     article_flow.kickoff()
